7f4d6577ff55ca57560d79801dc2a3d5a6ebfe68
srlucasdeoliveira <163764192+srlucasdeoliveira@users.noreply.github.com>
2025-02-25 14:11:54 -0300

    Update site.js

--- Nova pasta/site.js
+++ Nova pasta/site.js
@@ -1,7 +1,24 @@
-// Menu hamburguer para dispositivos móveis
+// Menu Hamburguer Responsivo
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+});
+
+// Rolagem Suave para as Seções
+const links = document.querySelectorAll('.nav-links a');
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const targetId = link.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop - 50,  // Ajuste para o topo, considerando o cabeçalho fixo
+      behavior: 'smooth'
+    });
+  });
 });
