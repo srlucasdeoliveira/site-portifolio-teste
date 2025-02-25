79b9c9b6a02adbfbba6e7674ed9d8f63f1596dcf
srlucasdeoliveira <163764192+srlucasdeoliveira@users.noreply.github.com>
2025-02-25 14:05:35 -0300

    Add files via upload

---
+++ Nova pasta/site.js
@@ -0,0 +1,7 @@
+// Menu hamburguer para dispositivos móveis
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
